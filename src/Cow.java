54e9502b57da2c9702a53c64567a8b1eef40a80e
iancurranschoolgithub <ianc2004@yahoo.com>
2022-02-23 14:36:29 -0500

    added previous work

---
+++ src/Cow.java
@@ -0,0 +1,19 @@
+    public class Cow implements Animal {
+        private String myType;
+        private String mySound;
+
+        Cow()
+        {
+            myType = "cow";
+            mySound = "moo";
+        }
+
+        public String getSound()
+        {
+            return mySound;
+        }
+
+        public String getType() {
+            return myType;
+        }
+    }
---
+++ src/Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+    public static void main(String[] args){
+        Cow c = new Cow();
+        System.out.println( c.getType() + " goes " + c.getSound() );
+        // < your code here >
+    }
+}
