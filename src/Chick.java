3e4043fb7592f336d0a388e111aa2c0ac62186e4
ianc2004@yahoo.com <iancurranschoolgithub>
2022-02-25 10:43:15 -0500

    Made the Chick getSound() more efficient

--- src/Chick.java
+++ src/Chick.java
@@ -4,25 +4,19 @@ public class Chick implements Animal {
     private String myType;
     private String mySound;
 
-    public String determineSounds()
-    {
-        String chickSounds;
-        double x = 2*Math.random();
-        if (x >= 1){
-            chickSounds = "cheep";
-        }
-        else chickSounds = "cluck";
-        return chickSounds;
-    }
-
     Chick()
     {
         myType = "chick";
-        mySound = determineSounds();
+        mySound = "cluck";
     }
 
     public String getSound()
     {
+
+        double x = 2*Math.random();
+        if (x >= 1){
+            mySound = "cheep";
+        }
         return mySound;
     }
 
