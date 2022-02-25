5cf35e7f84c46a927aca34e0d69d9d8e9248a2ff
ianc2004@yahoo.com <iancurranschoolgithub>
2022-02-25 10:46:55 -0500

    Deleted Tester.java; Lab Completed

--- src/Tester.java
+++
@@ -1,7 +0,0 @@
-public class Tester{
-    public static void main(String[] args){
-        Pig c = new Pig();
-        System.out.println( c.getType() + " goes " + c.getSound() );
-        // < your code here >
-    }
-}
